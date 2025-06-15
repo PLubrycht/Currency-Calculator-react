e3fe22b2da8107931e2f7a59f5456380ff1323ba
Pawel Lubrycht <plubrycht@gmail.com>
2025-06-15 12:27:27 +0200

    project update, tests added, code updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import "./App.css";
+import VantaBackground from "./components/VantaBackground/VantaBackground";
 import Container from "./components/Container/container";
 
 function App() {
-  return <Container />;
+  return (
+    <>
+      <VantaBackground />
+      <Container />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/VantaBackground/VantaBackground.jsx
@@ -0,0 +1,51 @@
+import { useEffect, useRef, useState } from "react";
+import * as THREE from "three";
+import NET from "vanta/dist/vanta.net.min";
+
+const VantaBackground = () => {
+  const vantaRef = useRef(null);
+  const [vantaEffect, setVantaEffect] = useState(null);
+
+  useEffect(() => {
+    if (!vantaEffect) {
+      setVantaEffect(
+        NET({
+          el: vantaRef.current,
+          THREE,
+          mouseControls: true,
+          touchControls: true,
+          gyroControls: false,
+          minHeight: 200.0,
+          minWidth: 200.0,
+          scale: 1.0,
+          scaleMobile: 1.0,
+          color: 0x3ff3ff,
+          backgroundColor: 0x23153c,
+          spacing: 12.0,
+          maxDistance: 27.0,
+        })
+      );
+    }
+
+    return () => {
+      if (vantaEffect) vantaEffect.destroy();
+    };
+  }, [vantaEffect]);
+
+  return (
+    <div
+      ref={vantaRef}
+      style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        width: "100vw",
+        height: "100vh",
+        zIndex: 1,
+        pointerEvents: "none",
+      }}
+    />
+  );
+};
+
+export default VantaBackground;
